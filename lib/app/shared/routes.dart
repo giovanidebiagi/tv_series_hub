306a537d590801477143b01ace1a9a9b71c74a77
Giovani Debiagi <giovanibbiagi@gmail.com>
2022-04-22 20:35:41 -0300

    Removed some unused imports and enabled DevicePreview for debug version

--- lib/app/shared/routes.dart
+++ lib/app/shared/routes.dart
@@ -1,5 +1,3 @@
-import 'package:tv_series_hub/app/modules/my_favorite_tv_series/my_favorite_tv_series_page.dart';
-
 class Routes {
   static const homePage = '/';
   static const searchPage = '/search_page';
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:tv_series_hub/app/app_widget.dart';
 import 'package:tv_series_hub/app/data/repositories/local_storage_repositories/hive_repository.dart';
@@ -14,11 +13,9 @@ void main() async {
   await hiveRepository.init(directory: directory);
 
   runApp(
-    // DevicePreview(
-    // enabled: !kReleaseMode,
-    // builder: (context) =>
-
-    AppWidget(iLocalStorageRepository: hiveRepository),
-    // ),
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => AppWidget(iLocalStorageRepository: hiveRepository),
+    ),
   );
 }
